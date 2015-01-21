805b679b38480948b0244f8a40f2b737f73e9233
stil <lipvig@lipvig.net>
2015-01-22 00:40:17 +0100

    Fixed bugs with session validation

--- src/Client/Session.php
+++ src/Client/Session.php
@@ -2,24 +2,34 @@
 namespace Erpk\Harvester\Client;
 
 use GuzzleHttp\Cookie\FileCookieJar;
+use GuzzleHttp\Cookie\SetCookie;
 
 class Session
 {
+    /**
+     * @var string
+     */
     protected $savePath;
-    protected $authTimeout = 480;
+
+    /**
+     * @var FileCookieJar
+     */
     protected $cookieJar;
+
+    /**
+     * @var array|mixed
+     */
     protected $data = [
-        'touch'        =>  null,
-        'token'        =>  null,
-        'citizen.id'   =>  null,
-        'citizen.name' =>  null
+        'touch'     =>  null,
+        'token'     =>  null,
+        'citizen'   => ['id' => null, 'name' => null]
     ];
     
     public function __construct($savePath)
     {
         $this->savePath = $savePath;
 
-        if (file_exists($savePath)) {
+        if (file_exists($savePath.'.sess')) {
             $this->data = unserialize(file_get_contents($savePath.'.sess'));
         }
 
@@ -36,36 +46,61 @@ class Session
     {
         $this->save();
     }
-    
+
+    /**
+     * @return bool
+     */
     public function isValid()
     {
-        return (time()-$this->data['touch']) < $this->authTimeout;
+        if ($this->getToken() == null) {
+            return false;
+        }
+
+        foreach ($this->getCookieJar() as $cookie) {
+            /**
+             * @var SetCookie $cookie
+             */
+            if ($cookie->getName() == 'erpk' && $cookie->getDomain() == '.erepublik.com') {
+                return $cookie->getExpires() - time() > 30; // more than 30 seconds to expire
+            }
+        }
+
+        return false;
     }
-    
+
+    /**
+     * @return FileCookieJar
+     */
     public function getCookieJar()
     {
         return $this->cookieJar;
     }
-    
+
+    /**
+     * @return string
+     */
     public function getCitizenName()
     {
-        return $this->data['name'];
+        return $this->data['citizen']['name'];
     }
 
     public function setCitizenName($name)
     {
-        $this->data['citizen.name'] = $name;
+        $this->data['citizen']['name'] = $name;
         return $this;
     }
-    
+
+    /**
+     * @return int
+     */
     public function getCitizenId()
     {
-        return $this->data['id'];
+        return $this->data['citizen']['id'];
     }
 
     public function setCitizenId($id)
     {
-        $this->data['citizen.id'] = $id;
+        $this->data['citizen']['id'] = $id;
         return $this;
     }
 
@@ -80,12 +115,6 @@ class Session
     public function setToken($token)
     {
         $this->data['token'] = $token;
-        $this->touch();
         return $this;
-    }
-    
-    public function touch()
-    {
-        $this->data['touch'] = time();
     }
 }
